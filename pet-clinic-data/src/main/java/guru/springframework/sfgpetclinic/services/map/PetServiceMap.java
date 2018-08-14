cb4ea8baa921224b4e7d28a74c81ce6dae363daa
Valentin Matignon <valentinmatignon@gmail.com>
2018-08-14 11:29:57 +0200

    Adding Spring config. Closes #22.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap  extends AbtractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAdd() {
